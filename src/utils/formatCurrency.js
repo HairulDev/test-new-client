31f4321a18e27a90fd562746962c5f1883a35d85
Hairul <98758226+HairulDev@users.noreply.github.com>
2023-11-26 12:05:15 +0700

    redeploy

--- src/utils/formatCurrency.js
+++
@@ -1,7 +0,0 @@
-export default function convertToRupiah(value) {
-    const rupiah = new Intl.NumberFormat("id-ID", {
-        style: "currency",
-        currency: "IDR",
-    }).format(value);
-    return rupiah.replace(",00", "");
-}
